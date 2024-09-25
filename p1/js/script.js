b6faa636bc93edd30966ab9b5ca2f2c5fd3f2daa
Ali Feyzi <alifeyzi0082gmail.com>
2024-09-25 09:06:55 -0700

    one commit or push full file

---
+++ p1/js/script.js
@@ -0,0 +1,36 @@
+﻿/*
+
+ */
+
+/*شبیه سازی دیتابیس*/
+var users = [
+	{id: 1, name: 'ali', family: 'atomic', age: 22,email: 'atom@atom.com'},
+	{id: 2, name: 'hany', family: 'atomic', age: 27,email: 'hany@atom.com'},
+	{id: 3, name: 'many', family: 'atomic', age: 24,email: 'many@atom.com'},
+]
+
+/*
+گرفتن ورودی از کاربر و بررسی شرایط 
+*/
+var userName = prompt('Enter your Name:')
+var userFamilyName = prompt('Enter your Family Name:')
+var userAge = prompt('Enter your age:')
+var userEmail = prompt('Enter your email:')
+
+if(userName.length < 3 || userName > 10){
+	alert ("نام شما حداقل 3 و 10 کارکتر میتواند داشته باشد ")
+}else if(userFamilyName.length < 3 || userFamilyName > 10 ){
+	alert ("نام خانوادگی شما حداقل 3 و 10 کارکتر میتواند داشته باشد ")
+}else if (isNaN(userAge) || userAge.length > 4){
+	console.log("لطفا سن را به درسی وارد کنید")
+}else{
+	var newUer ={
+		id : 4,
+		name : userName,
+		family : userFamilyName,
+		age : userAge,
+		email : userEmail
+	}
+	users.push(newUer)
+	console.log(users)
+}
---
+++ p3/js/script.js
@@ -0,0 +1,14 @@
+﻿/*
+
+ */
+
+/*شبیه سازی دیتابیس*/
+var usersList = [
+	{id: 1, name: 'ali', family: 'atomic', age: 22,email: 'atom@atom.com'},
+	{id: 2, name: 'hany', family: 'atomic', age: 27,email: 'hany@atom.com'},
+	{id: 3, name: 'many', family: 'atomic', age: 24,email: 'many@atom.com'},
+]
+
+usersList.forEach(function(user){
+	console.log("user name : " + user.name +"user family :"  + user.family)
+})
