4ef176a85fdee27fd764cec0cd85706380b174fb
Ali Feyzi <alifeyzi0082gmail.com>
2024-09-27 01:24:54 -0700

    completion mini project 7

---
+++ p7/js/script.js
@@ -0,0 +1,20 @@
+﻿/*
+
+ */
+
+/*از کاربر کلمه مورد نظرشو میگیریم */
+var word = prompt ('کلمه خودتون رو وارد کنید');
+
+/*عملیات ریورس و چک کردن این که کلمه رفت و برگشتی یکسان است یا خیر */
+var characterArray = word.split('');
+
+var reverseCharacter = characterArray.reverse();
+var reverseWord = reverseCharacter.join();
+
+
+if(word === reverseWord){
+	alert("از هر دو جهت به یک شکل خوانده میشود");
+}else{
+	alert("از هر دو جهت به یک شکل خوانده نمیشود ");
+}
+
