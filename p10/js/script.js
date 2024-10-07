8b2c77ebe80be5b2e179c5992db7e5545c5eaadb
Ali Feyzi <alifeyzi0082gmail.com>
2024-10-07 05:27:17 -0700

    completion mini project 10

---
+++ p10/js/script.js
@@ -0,0 +1,20 @@
+﻿/*
+
+ */
+/*مبدا سفر را از کاربر گرفته*/
+
+var cities = {
+	tehrean:['tehrean','shahryar','eslamshahr','pardis','bumehen'],
+	shiraz:['shiraz','fars','jamshid','shiraz pars'],
+	mashhad:['mashhad','fariman','guchan'],
+	tabriz:['tabriz','marand','jolfa','ahar']
+}
+
+
+var startProvine = prompt('استان مورد نظر خود را وارد کنید');
+
+var mainProvinceCities = cities[startProvine];
+
+mainProvinceCities.forEach(function(city){
+	console.log(city)
+});
