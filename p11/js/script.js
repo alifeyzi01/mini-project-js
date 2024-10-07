6786f752f17bb990af9dd5a7ac85a632540c5d84
Ali Feyzi <alifeyzi0082gmail.com>
2024-10-07 05:54:48 -0700

    completion mini project 11

---
+++ p11/js/script.js
@@ -0,0 +1,24 @@
+﻿/*
+
+ */
+/*سوالاتی که از کاربر باید پرسیده شود*/
+var allQuestions = [
+	{id: 1, title:'2+3', answer: '5'},
+	{id: 1, title:'2^3', answer: '8'},
+	{id: 1, title:'3-2', answer: '1'},
+	{id: 1, title:'4/2', answer: '2'},
+	{id: 1, title:'4*5', answer: '20'},
+	{id: 1, title:'100/25', answer: '4'},
+	{id: 1, title:'cpaital of IRAN', answer: 'tehran'},
+];
+
+var score = 0;
+var mainAnswer = '';
+allQuestions.forEach(function(question){
+	mainAnswer = prompt(question.title + ' ?');
+
+	if(mainAnswer === question.answer){
+		score++;
+	}
+});
+alert('your final score: ' + score);
