d2b83de83cbf008ea3610982c2239ea14c000e75
Ali Feyzi <alifeyzi0082gmail.com>
2024-10-07 04:57:17 -0700

    completion mini project 9

---
+++ p9/js/script.js
@@ -0,0 +1,21 @@
+﻿/*
+
+ */
+/* لیست کاربران و وظایفشان*/
+var tasksArray = {
+	atom:['html','css','js'],
+	sina:['admin','seo'],
+	amir:['content','vue','react'],
+	salar:['link building','git push']
+}
+
+/*از مدیر نام کارمند و تسکی که میخواهد اضافه شود را میگیریم*/
+
+var newTaskName = prompt('تسک مورد نظرتان را وارد کنید:');
+var employName = prompt('کارمند مورد نظرتان را وارد کنید:');
+
+var employTasks = tasksArray[employName];
+employTasks.push(newTaskName);
+
+console.log(tasksArray);
+
